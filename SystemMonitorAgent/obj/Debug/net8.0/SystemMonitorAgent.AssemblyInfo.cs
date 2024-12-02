afb5e8f3db813ce49f89c481f4b7b0ff521f073f
joaojhgs <joaosperandio@alunos.utfpr.edu.br>
2024-12-02 17:12:54 -0300

    fix: dlls

--- SystemMonitorAgent/obj/Debug/net8.0/SystemMonitorAgent.AssemblyInfo.cs
+++ SystemMonitorAgent/obj/Debug/net8.0/SystemMonitorAgent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemMonitorAgent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3694b0693b2d29f09f7c2e9de3f241dcf043d44f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemMonitorAgent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemMonitorAgent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
