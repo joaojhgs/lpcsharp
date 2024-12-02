0424b554aded2309b8832eeefcb2cc8647c38d95
joaojhgs <joaosperandio@alunos.utfpr.edu.br>
2024-12-02 17:12:30 -0300

    fix: charts

--- ServerCentral/obj/Debug/net8.0/ServerCentral.AssemblyInfo.cs
+++ ServerCentral/obj/Debug/net8.0/ServerCentral.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCentral")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d260c6891ed6753b944970f8e6c88881f0000ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3694b0693b2d29f09f7c2e9de3f241dcf043d44f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCentral")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCentral")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
